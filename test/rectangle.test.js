d50bb8be8bbe7adda14165c851672a2c604f0bea
Mattis Tajan <mattis.tajan@ecoles-epsi.net>
2026-04-28 16:55:36 +0200

    Création du fichier de test du Rectangle et creation premier test

---
+++ test/rectangle.test.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const Rectangle = require('../src/rectangle');
+
+describe('Rectangle', () => {
+  it('devrait créer un rectangle avec des dimensions valides', () => {
+    const rect = new Rectangle(10, 5);
+    expect(rect.width).to.equal(10);
+    expect(rect.height).to.equal(5);
+  });
+
+  it('devrait lever une erreur si une dimension est négative', () => {
+    expect(() => new Rectangle(-1, 5)).to.throw('Les dimensions doivent être positives');
+  });
+});
